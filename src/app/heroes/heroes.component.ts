81b73655d6266b733cf1c21b38055d71c88a7245
Johan Smarius <jsmarius@hotmail.com>
2018-11-07 17:16:54 +0100

    Added lesson 4

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -14,16 +15,22 @@ export class HeroesComponent implements OnInit {
   //   id: 1,
   //   name: 'Windstorm'
   // };
-  heroes = HEROES;
+  heroes: Hero[];
   selectedHero: Hero;
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
   onSelect(hero: Hero){
     this.selectedHero = hero;
+  }
+
+  private getHeroes(): void {
+    this.heroService.getHeroes().
+      subscribe(heroes => this.heroes = heroes);
   }
 
 }
